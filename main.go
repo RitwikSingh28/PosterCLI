35a6776d43dfbe6d03e54053b9d626b86fbbc682
RitwikSingh28 <itsritwik26@gmail.com>
2024-05-21 13:21:38 +0530

    add modularity via functions

--- main.go
+++ main.go
@@ -1,4 +1,3 @@
-// Todo: make it modular, and robust
 package main
 
 import (
@@ -9,20 +8,36 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"path/filepath"
 )
 
-func main() {
-	title := os.Args[1:]
+type Movie struct {
+	Title  string `json:"Title"`
+	Year   string `json:"Year"`
+	Poster string `json:"Poster"`
+}
+
+type SearchResult struct {
+	Search []Movie `json:"Search"`
+}
+
+const apiKey = "1b8a3f2d"
+
+func parseMovieTitle(title []string) string {
 	movieTitle := ""
 	for _, partTitle := range title {
 		movieTitle += partTitle + " "
 	}
 	movieTitle = movieTitle[:len(movieTitle)-1]
 	encodedTitle := url.QueryEscape(movieTitle)
-	url := "http://www.omdbapi.com/?s=" + encodedTitle + "&apikey=1b8a3f2d"
+	url := "http://www.omdbapi.com/?s=" + encodedTitle + "&apikey=" + apiKey
+	return url
 
+}
+
+func fetchMovieData(title string) *SearchResult {
 	fmt.Println("Fetching Data....")
-	resp, err := http.Get(url)
+	resp, err := http.Get(title)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -37,14 +52,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Fprint(os.Stdin, "")
-	type SearchResult struct {
-		Search []struct {
-			Title  string `json:"Title"`
-			Year   string `json:"Year"`
-			Poster string `json:"Poster"`
-		} `json:"Search"`
-	}
 	var poster SearchResult
 
 	err = json.Unmarshal(data, &poster)
@@ -52,22 +59,27 @@ func main() {
 		log.Fatal(err)
 	}
 
-	if len(poster.Search) == 0 {
-		log.Fatal("No records found")
-		return
-	}
+	return &poster
+}
 
+func fetchAndSavePoster(poster *SearchResult) {
 	fmt.Println("Record Found.....")
 	fmt.Println("Downloading Image.....")
 	imgUrl := poster.Search[0].Poster
 	movTitle := poster.Search[0].Title
-	resp, err = http.Get(imgUrl)
+	resp, err := http.Get(imgUrl)
 	if err != nil {
 		log.Fatal("An error occurred when fetching image: ", err)
 	}
 	defer resp.Body.Close()
 
-	filePath := "/home/ritro015/Downloads/" + movTitle + ".jpg"
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal("Failed to get user's home directory: ", err)
+	}
+
+	downloadsDir := filepath.Join(homeDir, "Downloads")
+	filePath := filepath.Join(downloadsDir, movTitle+".jpg")
 	file, err := os.Create(filePath)
 	if err != nil {
 		log.Fatal("An error occurred during file creation: ", err)
@@ -79,4 +91,20 @@ func main() {
 		log.Fatal("Error: ", err)
 	}
 	fmt.Println("Successfully saved in ", filePath)
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Please provide a movie title")
+		return
+	}
+
+	title := parseMovieTitle(os.Args[1:])
+	poster := fetchMovieData(title)
+
+	if len(poster.Search) == 0 {
+		log.Fatal("No records found")
+		return
+	}
+	fetchAndSavePoster(poster)
 }
